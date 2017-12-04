9a7b0b87e1771b64612afeeb418f51cf22ea32e0
Benjamin Below <Benjamin.Below@softwire.com>
2017-12-04 09:00:07 +0000

    Day 4, part 2

--- src/Day 4/day4.js
+++ src/Day 4/day4.js
@@ -5,9 +5,15 @@ const isValidPassphrase = input => {
   return _.uniq(words).length === words.length;
 };
 
-const numberOfValidPhrases = input => {
+const isValidPartTwoPassphrase = input => {
+  let words = input.split(' ');
+  console.log(words);
+  return _.uniq(_.map(words, w => w.split('').sort().join())).length === words.length;
+};
+
+const numberOfValidPhrases = (input, predicate) => {
   return _.reduce(input.split('\n'), (sum, x) => {
-    if (isValidPassphrase(x)) {
+    if (predicate(x)) {
       return sum + 1;
     }
     return sum;
@@ -16,5 +22,6 @@ const numberOfValidPhrases = input => {
 
 module.exports = {
   isValidPassphrase,
-  numberOfValidPhrases
+  numberOfValidPhrases,
+  isValidPartTwoPassphrase
 };
