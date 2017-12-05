b90ff4fe5cc9a903b6333390512acdcc791b252d
Benjamin Below <Benjamin.Below@softwire.com>
2017-12-05 05:38:02 +0000

    Day 5

--- src/Day 5/day5.js
+++ src/Day 5/day5.js
@@ -1,11 +1,65 @@
 const _ = require('lodash');
 
+const generator = function* (input) {
+  const inputs = _.map(input.split('\n'), i => parseInt(i));
+  const modifications = _.map(inputs, i => 0);
+  let index = 0;
+
+  while (true) {
+    const oldIndex = _.clone(index);
+    index = index + inputs[index] + modifications[index];
+    modifications[oldIndex] = modifications[oldIndex] + 1;
+    yield index;
+  }
+};
+
+const generator2 = function* (input) {
+  const inputs = _.map(input.split('\n'), i => parseInt(i));
+  const modifications = _.map(inputs, i => 0);
+  let index = 0;
+
+  while (true) {
+    const oldIndex = _.clone(index);
+    index = index + inputs[index] + modifications[index];
+    if(inputs[oldIndex] + modifications[oldIndex] >= 3) {
+      modifications[oldIndex] = modifications[oldIndex] - 1;
+    } else {
+      modifications[oldIndex] = modifications[oldIndex] + 1;
+    }
+    yield index;
+  }
+};
+
 const part1 = (input) => {
-  return input;
+  const inputs = _.map(input.split('\n'), i => parseInt(i));
+
+  let index = 0;
+  let count = 0;
+
+  const g = generator(input);
+
+  while ((index >= 0 && index < inputs.length)) {
+    index = g.next().value;
+    count += 1;
+  }
+
+  return count;
 };
 
 const part2 = (input) => {
-  return input;
+  const inputs = _.map(input.split('\n'), i => parseInt(i));
+
+  let index = 0;
+  let count = 0;
+
+  const g = generator2(input);
+
+  while ((index >= 0 && index < inputs.length)) {
+    index = g.next().value;
+    count += 1;
+  }
+
+  return count;
 };
 
 module.exports = {
--- src/Day 5/day5.test.js
+++ src/Day 5/day5.test.js
@@ -1,30 +1,40 @@
 const part1 = require('./day5').part1;
 const part2 = require('./day5').part2;
 
-const finalInput = '';
+const finalInput = require('./input');
 
-describe('Day 5', () => {
+describe('Puzzle', () => {
   describe('Part 1', () => {
     describe('example cases', () => {
       it('example 1', () => {
-        expect(part1(0)).toBe(0);
+        expect(part1(`0
+3
+0
+1
+-3`)).toBe(5);
       });
     });
 
     it('final input', () => {
-      expect(part1(finalInput)).toBe('');
+      // Long running (~1s)
+      // expect(part1(finalInput)).toBe(378980);
     });
   });
 
   describe('Part 2', () => {
     describe('example cases', () => {
       it('example 1', () => {
-        expect(part2(0)).toBe(0);
+        expect(part2(`0
+3
+0
+1
+-3`)).toBe(10);
       });
     });
 
     it('final input', () => {
-      expect(part2(finalInput)).toBe('');
+      // Long running (~70s)
+      // expect(part2(finalInput)).toBe(26889114);
     });
   })
 });
