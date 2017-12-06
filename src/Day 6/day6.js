cc9b4e72a80b70928242caae7ac9c2cadee12754
Benjamin Below <Benjamin.Below@softwire.com>
2017-12-06 05:49:25 +0000

    Day 6 part 2

--- src/Day 6/day6.js
+++ src/Day 6/day6.js
@@ -43,7 +43,43 @@ const part1 = (input) => {
 };
 
 const part2 = (input) => {
-  return input;
+  const numbers = _.map(input.split(/ |\t+/), i => parseInt(i));
+  let i = 0;
+
+  console.log(numbers);
+
+  const seenStates = [_.clone(numbers)];
+
+  while ((_.uniqWith(seenStates, (x, y) => x.compare(y)).length === seenStates.length)) {
+    const highestIndex = _.reduce(numbers, (s, n, i) => {
+      if (n > numbers[s]) {
+        return i;
+      } else {
+        return s;
+      }
+    }, 0);
+    const highest = Object.assign({}, {value: numbers[highestIndex] });
+
+    let currentIndex = highestIndex;
+    numbers[highestIndex] = 0;
+    for(let j = 1; j <= highest.value; j+=1) {
+      currentIndex = (currentIndex + 1) % numbers.length;
+      numbers[currentIndex] += 1;
+    }
+
+    seenStates.push(_.clone(numbers));
+    i++;
+  }
+
+  let firstIndex;
+  _.each(seenStates, (s, i) => {
+    if(s.compare(_.last(seenStates)) && !firstIndex) {
+      firstIndex = i;
+      console.log(firstIndex);
+    }
+  });
+
+  return seenStates.length -1 - firstIndex;
 };
 
 module.exports = {
--- src/Day 6/day6.test.js
+++ src/Day 6/day6.test.js
@@ -3,14 +3,12 @@ const part2 = require('./day6').part2;
 
 const finalInput = require('./input');
 
-const example = ``;
+const input = `0, 2, 7, 0`;
 
 describe('Puzzle', () => {
   describe('Part 1', () => {
     describe('example cases', () => {
       it('example 1', () => {
-        const input = `0, 2, 7, 0`;
-
         expect(part1(input)).toBe(5);
       });
     });
@@ -23,14 +21,12 @@ describe('Puzzle', () => {
   describe('Part 2', () => {
     describe('example cases', () => {
       it('example 1', () => {
-        const input = ``;
-
-        expect(part2(0)).toBe(0);
+        expect(part2(input)).toBe(4);
       });
     });
 
     it('final input', () => {
-      // expect(part2(finalInput)).toBe('');
+      // expect(part2(finalInput)).toBe(1610);
     });
   })
 });
