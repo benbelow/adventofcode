18ef612961fe121cc934d6cc51cabc2598ac58e3
Benjamin Below <Benjamin.Below@softwire.com>
2017-12-08 15:30:22 +0000

    Add a few days worth of templates

---
+++ src/Day 11/puzzle.test.js
@@ -0,0 +1,30 @@
+const part1 = require('./puzzle').part1;
+const part2 = require('./puzzle').part2;
+
+const finalInput = require('./input');
+
+describe('Puzzle', () => {
+  describe('Part 1', () => {
+    describe('example cases', () => {
+      it('example 1', () => {
+        expect(part1(0)).toBe(0);
+      });
+    });
+
+    it('final input', () => {
+      expect(part1(finalInput)).toBe('');
+    });
+  });
+
+  describe('Part 2', () => {
+    describe('example cases', () => {
+      it('example 1', () => {
+        expect(part2(0)).toBe(0);
+      });
+    });
+
+    it('final input', () => {
+      expect(part2(finalInput)).toBe('');
+    });
+  })
+});
---
+++ src/Practice/2016/Day 8/puzzle.js
@@ -0,0 +1,14 @@
+const _ = require('lodash');
+
+const part1 = (input) => {
+  return input;
+};
+
+const part2 = (input) => {
+  return input;
+};
+
+module.exports = {
+  part1,
+  part2,
+};
