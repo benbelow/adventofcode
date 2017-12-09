1f5557ae2221f8da6b1643bfde9f0f7bbfef3fc7
Benjamin Below <Benjamin.Below@softwire.com>
2017-12-09 06:54:42 +0000

    Day 9, Part 1

--- src/Day 9/puzzle.js
+++ src/Day 9/puzzle.js
@@ -1,7 +1,63 @@
 const _ = require('lodash');
 
 const part1 = (input) => {
-  return input;
+
+  let score = 0;
+  let groupLevel = 0;
+  let isGarbage = false;
+  let shouldIgnoreNext = false;
+
+  const enterGroup = () => {
+    groupLevel++;
+    score += groupLevel;
+  };
+
+  const leaveGroup = () => {
+    groupLevel--;
+  };
+
+  const enterGarbage = () => {
+    isGarbage = true;
+  };
+
+  const leaveGarbage = () => {
+    isGarbage = false;
+  };
+
+  _.each(input, c => {
+    if(shouldIgnoreNext) {
+      shouldIgnoreNext = false;
+      return;
+    }
+
+    if (c === '{' && !isGarbage) {
+      enterGroup();
+      return;
+    }
+
+    if(c === '}' && !isGarbage) {
+      leaveGroup();
+      return;
+    }
+
+    if(c === '<' && !isGarbage) {
+      enterGarbage();
+      return;
+    }
+
+    if(c === '>' && isGarbage) {
+      leaveGarbage();
+      return;
+    }
+
+    if(c === '!' && isGarbage) {
+      shouldIgnoreNext = true;
+      return;
+    }
+
+  });
+
+  return score;
 };
 
 const part2 = (input) => {
--- src/Day 9/puzzle.test.js
+++ src/Day 9/puzzle.test.js
@@ -7,24 +7,82 @@ describe('Puzzle', () => {
   describe('Part 1', () => {
     describe('example cases', () => {
       it('example 1', () => {
-        expect(part1(0)).toBe(0);
+        expect(part1('{}')).toBe(1);
       });
+
+      it('example 1', () => {
+        expect(part1('{{{}}}')).toBe(6);
+      });
+
+      it('example 1', () => {
+        expect(part1('{{},{}}')).toBe(5);
+      });
+
+      it('example 1', () => {
+        expect(part1('{{{},{},{{}}}}')).toBe(16);
+      });
+
+      it('example 1', () => {
+        expect(part1('{<a>,<a>,<a>,<a>}')).toBe(1);
+      });
+
+      it('example 1', () => {
+        expect(part1('{{<ab>},{<ab>},{<ab>},{<ab>}}')).toBe(9);
+      });
+
+      it('example 1', () => {
+        expect(part1('{{<!!>},{<!!>},{<!!>},{<!!>}}')).toBe(9);
+      });
+
+      it('example 1', () => {
+        expect(part1('{{<a!>},{<a!>},{<a!>},{<ab>}}')).toBe(3);
+      });
+
+      it('example 1', () => {
+        expect(part1('{<{o"i!a,<{i<a>}')).toBe(1);
+      });
+
     });
 
     it('final input', () => {
-      expect(part1(finalInput)).toBe('');
+      expect(part1(finalInput)).toBe(10820);
     });
   });
 
   describe('Part 2', () => {
     describe('example cases', () => {
       it('example 1', () => {
-        expect(part2(0)).toBe(0);
+        expect(part2('<>')).toBe(0);
       });
+
+      it('example 1', () => {
+        expect(part2('<random characters>')).toBe(17);
+      });
+
+      it('example 1', () => {
+        expect(part2('<<<<>')).toBe(3);
+      });
+
+      it('example 1', () => {
+        expect(part2('<{!>}>')).toBe(2);
+      });
+
+      it('example 1', () => {
+        expect(part2('<!!>')).toBe(0);
+      });
+
+      it('example 1', () => {
+        expect(part2('<{o"i!a,<{i<a>')).toBe(10);
+      });
+
+      it('example 1', () => {
+        expect(part2('<!!!>>')).toBe(0);
+      });
+
     });
 
     it('final input', () => {
-      expect(part2(finalInput)).toBe('');
+      // expect(part2(finalInput)).toBe('');
     });
   })
 });
