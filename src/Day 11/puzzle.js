d4d59a266dca5817bdbdd7a5aaec2e63926180ad
Benjamin Below <Benjamin.Below@softwire.com>
2017-12-11 13:31:00 +0000

    Day 11

--- src/Day 11/puzzle.js
+++ src/Day 11/puzzle.js
@@ -1,11 +1,48 @@
 const _ = require('lodash');
 
+const stepTypes = {
+  'n': [0, 1, -1],
+  'ne': [1, 0, -1],
+  'se': [1, -1, 0],
+  's': [0, -1, 1],
+  'sw': [-1, 0, 1],
+  'nw': [-1, 1, 0],
+};
+
 const part1 = (input) => {
-  return input;
+  const steps = input.split(',');
+
+  const position = [0, 0, 0];
+
+  const applyStep = step => _.each(_.get(stepTypes, step), (s, i) => position[i] += s);
+
+  _.each(steps, s => applyStep(s));
+
+  console.log(steps);
+  console.log(position);
+
+  return _.sum(position.map(p => Math.abs(p)))/2;
 };
 
 const part2 = (input) => {
-  return input;
+  const steps = input.split(',');
+
+  const position = [0, 0, 0];
+
+  const applyStep = step => _.each(_.get(stepTypes, step), (s, i) => position[i] += s);
+
+  const distanceFromOrigin = () => _.sum(position.map(p => Math.abs(p)))/2;
+
+  let maxDistance = 0;
+
+  _.each(steps, s => {
+    applyStep(s);
+    if(distanceFromOrigin() > maxDistance) {
+      maxDistance = distanceFromOrigin();
+    }
+  });
+
+  return maxDistance;
 };
 
 module.exports = {
--- src/Day 11/puzzle.test.js
+++ src/Day 11/puzzle.test.js
@@ -7,24 +7,34 @@ describe('Puzzle', () => {
   describe('Part 1', () => {
     describe('example cases', () => {
       it('example 1', () => {
-        expect(part1(0)).toBe(0);
+        expect(part1('ne,ne,ne')).toBe(3);
       });
+
+      it('example 1', () => {
+        expect(part1('ne,ne,sw,sw')).toBe(0);
+      });
+
+
+      it('example 1', () => {
+        expect(part1('ne,ne,s,s')).toBe(2);
+      });
+
+
+      it('example 1', () => {
+        expect(part1('se,sw,se,sw,sw')).toBe(3);
+      });
+
     });
 
     it('final input', () => {
-      expect(part1(finalInput)).toBe('');
+      // expect(part1(finalInput)).toBe(675);
     });
   });
 
   describe('Part 2', () => {
-    describe('example cases', () => {
-      it('example 1', () => {
-        expect(part2(0)).toBe(0);
-      });
-    });
 
     it('final input', () => {
-      expect(part2(finalInput)).toBe('');
+      // expect(part2(finalInput)).toBe(1424);
     });
   })
 });
