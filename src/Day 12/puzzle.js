21f8a67e087a0e380ce0692db3e39ec1398207a3
Benjamin Below <Benjamin.Below@softwire.com>
2017-12-12 05:49:25 +0000

    Day 12, part 1

--- src/Day 12/puzzle.js
+++ src/Day 12/puzzle.js
@@ -1,7 +1,33 @@
 const _ = require('lodash');
 
 const part1 = (input) => {
-  return input;
+  let pipes = _.map(input.split('\n'), p => {
+    const separated = p.split('<->');
+    return {
+      start: parseInt(separated[0]),
+      ends: _.map(separated[1].split(','), x => parseInt(x)),
+    };
+  });
+
+  const pipeAt = x => _.find(pipes, p => p.start === x);
+
+  const determineConnections = pipes => {
+    return _.map(pipes, p => {
+      const visited = [p.start];
+
+      const connections = pipe => {
+        visited.push(pipe.start);
+        let otherConnections = _.flatten(_.map(_.filter(pipe.ends, p => !visited.includes(p)), x => connections(pipeAt(x))));
+        return _.uniq(_.concat([], otherConnections, [pipe.start]));
+      };
+
+      return _.merge({}, p, {connected: connections(p)});
+    })
+  };
+
+  pipes = determineConnections(pipes);
+
+  return _.filter(pipes, p => p.connected.includes(0)).length;
 };
 
 const part2 = (input) => {
--- src/Day 12/puzzle.test.js
+++ src/Day 12/puzzle.test.js
@@ -3,28 +3,36 @@ const part2 = require('./puzzle').part2;
 
 const finalInput = require('./input');
 
+const example1 = `0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5`;
+
 describe('Puzzle', () => {
   describe('Part 1', () => {
     describe('example cases', () => {
       it('example 1', () => {
-        expect(part1(0)).toBe(0);
+        expect(part1(example1)).toBe(6);
       });
     });
 
     it('final input', () => {
-      expect(part1(finalInput)).toBe('');
+      // expect(part1(finalInput)).toBe(134);
     });
   });
 
   describe('Part 2', () => {
     describe('example cases', () => {
       it('example 1', () => {
-        expect(part2(0)).toBe(0);
+        // expect(part2(0)).toBe(0);
       });
     });
 
     it('final input', () => {
-      expect(part2(finalInput)).toBe('');
+      // expect(part2(finalInput)).toBe('');
     });
   })
 });
