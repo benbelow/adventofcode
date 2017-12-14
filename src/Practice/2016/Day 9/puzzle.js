fd7eafc0bd10f0f4a5d68b8c550a5a2758a7cf68
Benjamin Below <Benjamin.Below@softwire.com>
2017-12-14 15:23:57 +0000

    (2016) Day 9, Part 2

--- src/Practice/2016/Day 9/puzzle.js
+++ src/Practice/2016/Day 9/puzzle.js
@@ -22,8 +22,28 @@ const part1 = input => {
   }, 0)
 };
 
-const part2 = (input) => {
-  return input;
+const part2 = input => {
+  let numberToIgnore = 0;
+
+  return _.reduce(input, (sum, c, i) => {
+    if (numberToIgnore > 0) {
+      numberToIgnore--;
+      return sum;
+    }
+
+    if (c === '(') {
+      const marker = input.slice(i, input.length).split('(')[1].split(')')[0];
+      const characters = parseInt(marker.split('x')[0]);
+      const repetitions = parseInt(marker.split('x')[1]);
+
+      // Add one for the closing bracket. Already accounted for opening bracket
+      numberToIgnore += (marker.length + 1) + characters;
+
+      return sum + (part2(input.slice(i + marker.length + 2, i + marker.length + 2 + characters)) * repetitions);
+    }
+
+    return sum + 1;
+  }, 0)
 };
 
 module.exports = {
--- src/Practice/2016/Day 9/puzzle.test.js
+++ src/Practice/2016/Day 9/puzzle.test.js
@@ -47,12 +47,24 @@ describe('Puzzle', () => {
   describe('Part 2', () => {
     describe('example cases', () => {
       it('example 1', () => {
-        // expect(part2(0)).toBe(0);
+        expect(part2('(3x3)XYZ')).toBe(9);
+      });
+
+      it('example 1', () => {
+        expect(part2('X(8x2)(3x3)ABCY')).toBe('XABCABCABCABCABCABCY'.length);
+      });
+
+      it('example 1', () => {
+        expect(part2('(27x12)(20x12)(13x14)(7x10)(1x12)A')).toBe(241920);
+      });
+
+      it('example 1', () => {
+        expect(part2('(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN')).toBe(445);
       });
     });
 
     it('final input', () => {
-      // expect(part2(finalInput)).toBe('');
+      // expect(part2(finalInput)).toBe(11125026826);
     });
   })
 });
