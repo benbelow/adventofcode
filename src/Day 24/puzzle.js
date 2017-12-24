ee8f015febfae05a70b77579082db88a019db98b
Benjamin Below <Benjamin.Below@softwire.com>
2017-12-24 05:58:20 +0000

    Day 24

---
+++ src/Day 24/puzzle.js
@@ -0,0 +1,59 @@
+const _ = require('lodash');
+
+const part1 = (input) => {
+  // const ports = _.sortBy(_.map(input.split('\n'), i => _.map(i.split('/'), q => parseInt(q)).sort()), x => x[0] * 10 + x[1]);
+  const ports = _.map(input.split('\n'), i => _.map(i.split('/'), q => parseInt(q)));
+  const pins = 0;
+
+  const options = (index, taken = []) => {
+    return _.filter(ports, (p, i) => (p[0] === index || p[1] === index) && !taken.includes(i))
+  };
+
+  const chains = [];
+
+  const calculateChain = (index, taken = []) => {
+    chains.push(taken);
+    _.forEach(options(index, taken), (o) => {
+      const next = o[0] === o[1] ? o[1] : _.find(o, i => i !== index);
+      calculateChain(next, _.concat([], taken, [_.indexOf(ports, o)]))
+    });
+  };
+
+  calculateChain(pins, []);
+
+  const sums = _.map(chains, (c, j) => _.reduce(c, (sum, i) => sum + ports[i][0] + ports[i][1], 0));
+  return _.max(sums);
+};
+
+const part2 = (input) => {
+  const ports = _.map(input.split('\n'), i => _.map(i.split('/'), q => parseInt(q)));
+  const pins = 0;
+
+  const options = (index, taken = []) => {
+    return _.filter(ports, (p, i) => (p[0] === index || p[1] === index) && !taken.includes(i))
+  };
+
+  const chains = [];
+
+  const calculateChain = (index, taken = []) => {
+    chains.push(taken);
+    _.forEach(options(index, taken), (o) => {
+      const next = o[0] === o[1] ? o[1] : _.find(o, i => i !== index);
+      calculateChain(next, _.concat([], taken, [_.indexOf(ports, o)]))
+    });
+  };
+
+  calculateChain(pins, []);
+
+  const lengths = _.map(chains, (c, i) => [i, c.length]);
+  const maxLength = _.maxBy(lengths, l => l[1])[1];
+
+  const sums = _.map(_.filter(chains, c => c.length === maxLength), (c, j) => _.reduce(c, (sum, i) => sum + ports[i][0] + ports[i][1], 0));
+
+  return _.max(sums);
+};
+
+module.exports = {
+  part1,
+  part2,
+};
---
+++ src/Day 24/puzzle.test.js
@@ -0,0 +1,38 @@
+const part1 = require('./puzzle').part1;
+const part2 = require('./puzzle').part2;
+
+const finalInput = require('./input');
+const example = `0/2
+2/2
+2/3
+3/4
+3/5
+0/1
+10/1
+9/10`;
+
+describe('Puzzle', () => {
+  describe('Part 1', () => {
+    describe('example cases', () => {
+      it('example 1', () => {
+        expect(part1(example)).toBe(31);
+      });
+    });
+
+    it('final input', () => {
+      // expect(part1(finalInput)).toBe(1906);
+    });
+  });
+
+  describe('Part 2', () => {
+    describe('example cases', () => {
+      it('example 1', () => {
+        expect(part2(example)).toBe(19);
+      });
+    });
+
+    it('final input', () => {
+      expect(part2(finalInput)).toBe('');
+    });
+  })
+});
