8d3b6c686df9e4c629c88a2a6fbafc51c44b2d43
Benjamin Below <Benjamin.Below@softwire.com>
2018-12-07 21:11:04 +0000

    day 7 part 2

--- src/2018/Day 7/puzzle.js
+++ src/2018/Day 7/puzzle.js
@@ -1,112 +1,117 @@
 const _ = require('lodash');
 
 function getNextNodes(nodes) {
-    // console.log(nodes, _.minBy(_.values(nodes), n => n.preReq.length));
-    const minLen = _.minBy(_.values(nodes), n => n.preReq.length).preReq.length;
+  // console.log(nodes, _.minBy(_.values(nodes), n => n.preReq.length));
+  const minLen = _.minBy(_.values(nodes), n => n.preReq.length).preReq.length;
 
-    return _.values(nodes).filter(n => n.preReq.length === minLen).map(x => x.name).sort();
+  return _.values(nodes).filter(n => n.preReq.length === minLen).map(x => x.name).sort();
 }
 
 const alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('').sort();
 
 function removeNode(nodes, nextNode) {
-    delete nodes[nextNode];
-    _.values(nodes).forEach(n => {
-        _.remove(n.preReq, p => p === nextNode);
-    });
+  delete nodes[nextNode];
+  _.values(nodes).forEach(n => {
+    _.remove(n.preReq, p => p === nextNode);
+  });
 }
 
+let parseNodes = function (instructions) {
+  const nodes = {};
+  for (let instruction of instructions) {
+    const words = instruction.split(' ');
+    const node = words[7];
+    const preRequisite = words[1];
+
+    if (!nodes[node]) {
+      nodes[node] = { name: node, preReq: [] }
+    }
+
+    if (!nodes[preRequisite]) {
+      nodes[preRequisite] = { name: preRequisite, preReq: [] }
+    }
+
+    nodes[node].preReq.push(preRequisite);
+  }
+  return nodes;
+};
+
 const part1 = (input) => {
-    const instructions = input.split('\n');
+  const instructions = input.split('\n');
 
-    const nodes = {};
+  const nodes = parseNodes(instructions);
 
-    for (let instruction of instructions) {
-        const words = instruction.split(' ');
-        const node = words[7];
-        const preRequisite = words[1];
+  let answer = [];
+  let failsafe = 0;
 
-        if (!nodes[node]) {
-            nodes[node] = {name: node, preReq: []}
-        }
+  while (!_.values(nodes).every(n => n.length === 0) && failsafe < 100) {
+    const nextNode = getNextNodes(nodes)[0];
 
-        if (!nodes[preRequisite]) {
-            nodes[preRequisite] = {name: preRequisite, preReq: []}
-        }
+    answer.push(nextNode);
+    removeNode(nodes, nextNode);
 
-        nodes[node].preReq.push(preRequisite);
-    }
+    failsafe++;
+  }
 
-    let answer = [];
-    let failsafe = 0;
-
-    while(!_.values(nodes).every(n => n.length === 0) && failsafe < 100) {
-        const nextNode = getNextNodes(nodes)[0];
-
-        answer.push(nextNode);
-        removeNode(nodes, nextNode);
-
-        failsafe++;
-    }
-
-    console.log(nodes);
-    return answer.join('');
+  return answer.join('');
 };
 
 const part2 = (input, workerNum = 5, paddingTime = 60) => {
-    const instructions = input.split('\n');
+  const instructions = input.split('\n');
 
-    const nodes = {};
+  const nodes = parseNodes(instructions);
 
-    for (let instruction of instructions) {
-        const words = instruction.split(' ');
-        const node = words[7];
-        const preRequisite = words[1];
+  const workers = [];
+  for (let i = 0; i < workerNum; i++) {
+    workers.push({ id: i, timeLeft: 0, node: null });
+  }
 
-        if (!nodes[node]) {
-            nodes[node] = {name: node, preReq: []}
-        }
+  function assignNode(worker, node) {
+    worker.node = node;
+    worker.timeLeft += alphabet.indexOf(node.toLowerCase()) + 1 + paddingTime;
+  }
 
-        if (!nodes[preRequisite]) {
-            nodes[preRequisite] = {name: preRequisite, preReq: []}
-        }
-
-        nodes[node].preReq.push(preRequisite);
+  let assignWorkers = function () {
+    const validNodes = _.values(nodes)
+      .filter(n => n.preReq.length === 0)
+      .filter(n => !workers.some(w => w.node === n.name))
+      .map(n => n.name)
+      .sort();
+    while (workers.some(w => w.node === null) && validNodes.length > 0) {
+      const worker = workers.find(w => w.node === null);
+      assignNode(worker, validNodes[0]);
+      validNodes.shift();
     }
+  };
 
-    let answer = [];
-    let failsafe = 100;
-    let workers = {};
+  function finishNode(node) {
+    removeNode(nodes, node);
+  }
 
-    for(let w = 0; w < workerNum; w++) {
-        workers[w] = {state: 0, node: null};
+  function doWork() {
+    workers.forEach(w => {
+      if (w.node) {
+        w.timeLeft--;
+      }
+      if (w.timeLeft <= 0) {
+        finishNode(w.node);
+        w.node = null;
+      }
+    })
+  }
+
+  for (let i = 1; i < 10000; i++) {
+    if (workers.some(w => w.node === null)) {
+      assignWorkers();
     }
-
-    for (let i = 0; i < failsafe; i++) {
-        for(let w in workers) {
-            let nextNodes = _.values(nodes).length > 0 ? getNextNodes(nodes) : [];
-            if (workers[w].state <= 0 && nextNodes.length > 0) {
-                workers[w].state = paddingTime + alphabet.indexOf(nextNodes[0].toLowerCase()) + 1;
-                workers[w].node = nextNodes[0];
-            }
-            workers[w].state--;
-
-            if (workers[w].state <= 0) {
-                removeNode(nodes, workers[w].node);
-                workers[w].node = null;
-            }
-        }
-        if (_.values(nodes).length === 0 && _.values(workers).every(w => w <= 0)) {
-            return i;
-        }
+    doWork();
+    if (_.values(nodes).length === 0) {
+      return i;
     }
-
-
-    console.log(nodes);
-    return answer.join('');
+  }
 };
 
 module.exports = {
-    part1,
-    part2,
+  part1,
+  part2,
 };
