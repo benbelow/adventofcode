d9f9791ef84e446a0a1e2bc9d98179626def9778
Ben Below <benjamin.below@softwire.com>
2019-12-12 10:46:14 +0000

    Day12 Part2 TooSlow

--- AdventOfCode.2019/Day12/Day12.cs
+++ AdventOfCode.2019/Day12/Day12.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using AdventOfCode._2019.Common;
 using AdventOfCode._2019.Common.Models;
 
@@ -13,7 +14,13 @@ namespace AdventOfCode._2019.Day12
             var lines = FileReader.ReadInputLines(12);
             return EnergyAfterXSteps(lines, 1000);
         }
-        
+
+        public static long Part2()
+        {
+            var lines = FileReader.ReadInputLines(12);
+            return StepsUntilRepeat(lines);
+        }
+
         public static int EnergyAfterXSteps(IEnumerable<string> moonStrings, int steps)
         {
             var moons = moonStrings.Select(ParseMoon).ToList();
@@ -25,6 +32,132 @@ namespace AdventOfCode._2019.Day12
             }
 
             return moons.Sum(m => m.Energy());
+        }
+
+        public static long StepsUntilRepeat(IEnumerable<string> moonStrings)
+        {
+            var moons = moonStrings.Select(ParseMoon).ToList();
+
+            var xAHistory = new Dictionary<Complex, bool>();
+            var xBHistory = new Dictionary<Complex, bool>();
+            var xCHistory = new Dictionary<Complex, bool>();
+            var xDHistory = new Dictionary<Complex, bool>();
+
+            var yAHistory = new Dictionary<Complex, bool>();
+            var yBHistory = new Dictionary<Complex, bool>();
+            var yCHistory = new Dictionary<Complex, bool>();
+            var yDHistory = new Dictionary<Complex, bool>();
+
+            var zAHistory = new Dictionary<Complex, bool>();
+            var zBHistory = new Dictionary<Complex, bool>();
+            var zCHistory = new Dictionary<Complex, bool>();
+            var zDHistory = new Dictionary<Complex, bool>();
+
+            xAHistory[new Complex(moons[0].Position.X, moons[0].Velocity.X)] = true;
+            xBHistory[new Complex(moons[1].Position.X, moons[1].Velocity.X)] = true;
+            xCHistory[new Complex(moons[2].Position.X, moons[2].Velocity.X)] = true;
+            xDHistory[new Complex(moons[3].Position.X, moons[3].Velocity.X)] = true;
+
+            yAHistory[new Complex(moons[0].Position.Y, moons[0].Velocity.Y)] = true;
+            yBHistory[new Complex(moons[1].Position.Y, moons[1].Velocity.Y)] = true;
+            yCHistory[new Complex(moons[2].Position.Y, moons[2].Velocity.Y)] = true;
+            yDHistory[new Complex(moons[3].Position.Y, moons[3].Velocity.Y)] = true;
+
+            zAHistory[new Complex(moons[0].Position.Z, moons[0].Velocity.Z)] = true;
+            zBHistory[new Complex(moons[1].Position.Z, moons[1].Velocity.Z)] = true;
+            zCHistory[new Complex(moons[2].Position.Z, moons[2].Velocity.Z)] = true;
+            zDHistory[new Complex(moons[3].Position.Z, moons[3].Velocity.Z)] = true;
+
+            moons.ForEach(m => m.CalculateGravity(moons));
+            moons.ForEach(m => m.ApplyGravity());
+            moons.ForEach(m => m.ApplyVelocity());
+
+            long steps = 1;
+
+            long? xPeriodicity = null;
+            long? yPeriodicity = null;
+            long? zPeriodicity = null;
+
+            do
+            {
+                var xTuple = new Tuple<Complex, Complex, Complex, Complex>(
+                    new Complex(moons[0].Position.X, moons[0].Velocity.X),
+                    new Complex(moons[1].Position.X, moons[1].Velocity.X),
+                    new Complex(moons[2].Position.X, moons[2].Velocity.X),
+                    new Complex(moons[3].Position.X, moons[3].Velocity.X)
+                );
+                var yTuple = new Tuple<Complex, Complex, Complex, Complex>(
+                    new Complex(moons[0].Position.Y, moons[0].Velocity.Y),
+                    new Complex(moons[1].Position.Y, moons[1].Velocity.Y),
+                    new Complex(moons[2].Position.Y, moons[2].Velocity.Y),
+                    new Complex(moons[3].Position.Y, moons[3].Velocity.Y)
+                );
+                var zTuple = new Tuple<Complex, Complex, Complex, Complex>(
+                    new Complex(moons[0].Position.Z, moons[0].Velocity.Z),
+                    new Complex(moons[1].Position.Z, moons[1].Velocity.Z),
+                    new Complex(moons[2].Position.Z, moons[2].Velocity.Z),
+                    new Complex(moons[3].Position.Z, moons[3].Velocity.Z)
+                );
+
+                var xA = new Complex(moons[0].Position.X, moons[0].Velocity.X);
+                var xB = new Complex(moons[1].Position.X, moons[1].Velocity.X);
+                var xC = new Complex(moons[2].Position.X, moons[2].Velocity.X);
+                var xD = new Complex(moons[3].Position.X, moons[3].Velocity.X);
+                xAHistory[xA] = true;
+                xAHistory[xB] = true;
+                xAHistory[xC] = true;
+                xAHistory[xD] = true;
+                var zA = new Complex(moons[0].Position.Z, moons[0].Velocity.Z);
+                var zB = new Complex(moons[1].Position.Z, moons[1].Velocity.Z);
+                var zC = new Complex(moons[2].Position.Z, moons[2].Velocity.Z);
+                var zD = new Complex(moons[3].Position.Z, moons[3].Velocity.Z);
+                zAHistory[zA] = true;
+                zAHistory[zB] = true;
+                zAHistory[zC] = true;
+                zAHistory[zD] = true;
+                var yA = new Complex(moons[0].Position.Y, moons[0].Velocity.Y);
+                var yB = new Complex(moons[1].Position.Y, moons[1].Velocity.Y);
+                var yC = new Complex(moons[2].Position.Y, moons[2].Velocity.Y);
+                var yD = new Complex(moons[3].Position.Y, moons[3].Velocity.Y);
+                yAHistory[yA] = true;
+                yAHistory[yB] = true;
+                yAHistory[yC] = true;
+                yAHistory[yD] = true;
+
+
+                if (xAHistory.TryGetValue(xA, out var nxa)
+                    && xBHistory.TryGetValue(xB, out var nxb)
+                    && xCHistory.TryGetValue(xC, out var nxc)
+                    && xDHistory.TryGetValue(xD, out var nxd)
+                    && xPeriodicity == null)
+                {
+                    xPeriodicity = steps;
+                }
+
+                if (yAHistory.TryGetValue(yA, out var nya)
+                    && yBHistory.TryGetValue(yB, out var nyb)
+                    && yCHistory.TryGetValue(yC, out var nyc)
+                    && yDHistory.TryGetValue(yD, out var nyd) && yPeriodicity == null)
+                {
+                    yPeriodicity = steps;
+                }
+
+                if (zAHistory.TryGetValue(zA, out var nza)
+                    && zBHistory.TryGetValue(zB, out var nzb)
+                    && zCHistory.TryGetValue(zC, out var nzc)
+                    && zDHistory.TryGetValue(zD, out var nzd) && zPeriodicity == null)
+                {
+                    zPeriodicity = steps;
+                }
+
+                moons.ForEach(m => m.CalculateGravity(moons));
+                moons.ForEach(m => m.ApplyGravity());
+                moons.ForEach(m => m.ApplyVelocity());
+
+                steps++;
+            } while (xPeriodicity == null || yPeriodicity == null || zPeriodicity == null);
+
+            return LCM(LCM(xPeriodicity.Value, yPeriodicity.Value), zPeriodicity.Value);
         }
 
         private static Moon ParseMoon(string arg)
@@ -39,7 +172,7 @@ namespace AdventOfCode._2019.Day12
         {
             public int Id { get; set; }
             public Coordinate3D Position { get; set; }
-            private Coordinate3D Velocity;
+            public Coordinate3D Velocity;
 
             private Coordinate3D StagingGravityChange;
 
@@ -47,7 +180,14 @@ namespace AdventOfCode._2019.Day12
             {
                 Id = IdGenerator.NextId();
                 Position = new Coordinate3D(x, y, z);
-                Velocity = new Coordinate3D(0,0,0);
+                Velocity = new Coordinate3D(0, 0, 0);
+            }
+
+            public Moon(Moon moon)
+            {
+                Id = moon.Id;
+                Position = new Coordinate3D(moon.Position.X, moon.Position.Y, moon.Position.Z);
+                Velocity = new Coordinate3D(moon.Velocity.X, moon.Velocity.Y, moon.Velocity.Z);
             }
 
             public Coordinate3D CalculateGravity(IEnumerable<Moon> moons)
@@ -119,6 +259,28 @@ namespace AdventOfCode._2019.Day12
                 id++;
                 return id;
             }
+        }
+
+        private static long GCD(long a, long b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            // Pull out remainders.
+            for (;;)
+            {
+                long remainder = a % b;
+                if (remainder == 0) return b;
+                a = b;
+                b = remainder;
+            }
+
+            ;
+        }
+
+        private static long LCM(long a, long b)
+        {
+            return a * b / GCD(a, b);
         }
     }
 }
--- AdventOfCode.2019/Day12/Day12Tests.cs
+++ AdventOfCode.2019/Day12/Day12Tests.cs
@@ -20,6 +20,19 @@ namespace AdventOfCode._2019.Day12
         }
         
         [Test]
+        public void Part2Example1()
+        {
+            const string moons = @"<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>";
+
+            var steps = Day12.StepsUntilRepeat(moons.Split("\n"));
+
+            steps.Should().Be(2772);
+        }
+        
+        [Test]
         public void Part1Example2()
         {
             const string moons = @"<x=-8, y=-10, z=0>
@@ -31,11 +44,30 @@ namespace AdventOfCode._2019.Day12
 
             energy.Should().Be(1940);
         }
+        
+        [Test]
+        public void Part2Example2()
+        {
+            const string moons = @"<x=-8, y=-10, z=0>
+<x=5, y=5, z=10>
+<x=2, y=-7, z=3>
+<x=9, y=-8, z=-3>";
+
+            var energy = Day12.StepsUntilRepeat(moons.Split("\n"));
+
+            energy.Should().Be(4686774924);
+        }
 
         [Test]
         public void Part1()
         {
             Day12.Part1().Should().Be(9743);
+        }
+        
+        [Test]
+        public void Part2()
+        {
+            Day12.Part2().Should().Be(9743);
         }
     }
 }
