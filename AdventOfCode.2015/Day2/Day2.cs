8c209139d77763683ac8d56ca688c80d4837fd4e
Ben Below <benjamin.below@softwire.com>
2020-11-22 17:41:59 +0000

    2015: Day 2 Part 2

--- AdventOfCode.2015/Day2/Day2.cs
+++ AdventOfCode.2015/Day2/Day2.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Common;
 
@@ -9,17 +10,23 @@ namespace AdventOfCode._2015.Day2
         public static int Part1()
         {
             var lines = FileReader.ReadInputLines(2);
-            return lines.Select(l =>
-                {
-                    var strings = l.Split('x').Select(int.Parse).ToList();
-                    return (strings[0], strings[1], strings[2]);
-                })
+            return ParseDimensions(lines)
                 .Aggregate(0, (total, dimensions) => total + PaperNecessary(dimensions.Item1, dimensions.Item2, dimensions.Item3));
+        }
+
+        private static IEnumerable<(int, int, int)> ParseDimensions(this IEnumerable<string> lines)
+        {
+            return lines.Select(l =>
+            {
+                var strings = l.Split('x').Select(int.Parse).ToList();
+                return (strings[0], strings[1], strings[2]);
+            });
         }
 
         public static int Part2()
         {
-            return 0;
+            var dimensions = FileReader.ReadInputLines(2).ParseDimensions();
+            return dimensions.Sum(x => RibbonNecessary(x.Item1, x.Item2, x.Item3));
         }
 
         public static int PaperNecessary(int width, int height, int length)
@@ -29,6 +36,17 @@ namespace AdventOfCode._2015.Day2
             var smallestSide = Math.Min(side1, Math.Min(side2, side3));
 
             return 2 * (side1 + side2 + side3) + smallestSide;
+        }
+
+        public static int RibbonNecessary(int width, int height, int length)
+        {
+            var dimensions = new[] {width, height, length};
+            var twoSmallest = dimensions.OrderBy(x => x).Take(2);
+            
+            var nonBow = twoSmallest.Sum() * 2;
+            var bow = width * height * length;
+
+            return nonBow + bow;
         }
     }
 }
--- AdventOfCode.2015/Day2/Day2Tests.cs
+++ AdventOfCode.2015/Day2/Day2Tests.cs
@@ -13,17 +13,23 @@ namespace AdventOfCode._2015.Day2
             Day2.PaperNecessary(w, h, l).Should().Be(expected);
         }
         
+        [TestCase(2, 3, 4, 34)]
+        [TestCase(1, 1, 10, 14)]
+        public void RibbonTests(int w, int h, int l, int expected)
+        {
+            Day2.RibbonNecessary(w, h, l).Should().Be(expected);
+        }
+        
         [Test]
         public void Part1()
         {
-            Day2.Part1().Should().BeGreaterThan(1050010);
-            Day2.Part1().Should().Be(0);
+            Day2.Part1().Should().Be(1606483);
         }
         
         [Test]
         public void Part2()
         {
-            Day2.Part2().Should().Be(0);
+            Day2.Part2().Should().Be(3842356);
         }
     }
 }
