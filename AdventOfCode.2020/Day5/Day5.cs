8e969a9c3fc75d0367d041fb67416ea7b42d82ea
Ben Below <benjamin.below@softwire.com>
2020-12-05 09:13:47 +0000

    2020 Day 5 - use binary for part 1, actualy program part 2

--- AdventOfCode.2020/Day5/Day5.cs
+++ AdventOfCode.2020/Day5/Day5.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using AdventOfCode.Common;
 
@@ -8,78 +9,22 @@ namespace AdventOfCode._2020.Day5
         public static long Part1()
         {
             var lines = FileReader.ReadInputLines(5).ToList();
-            var seatIds = lines.Select(l => SeatId(l)).OrderBy(x => x);
+            var seatIds = lines.Select(SeatId).OrderBy(x => x);
             return seatIds.Max();
         }
 
         public static long SeatId(string s)
         {
-            var minFtb = 0;
-            var maxFtb = 127;;
-            foreach (var c in s.Take(7))
-            {
-                var range = maxFtb - minFtb;
-                switch (c)
-                {
-                    case 'F':
-                        if (range == 1)
-                        {
-                            maxFtb = minFtb;
-                            break;
-                        }
-                        maxFtb = maxFtb - (range / 2) -1;
-                        break;
-                    case 'B':
-                        if (range == 1)
-                        {
-                            minFtb = maxFtb;
-                            break;
-                        }
-                        minFtb = minFtb + (range / 2)+ 1;
-                        break;
-                }
-            }
-            
-            
-            var minLtr = 0;
-            var maxLtr = 7;
-            
-            foreach (var c in s.Skip(7))
-            {
-                var range = maxLtr - minLtr;
-                switch (c)
-                {
-                    case 'L':
-                        if (range == 1)
-                        {
-                            maxLtr = minLtr;
-                            break;
-                        }
-                        maxLtr = maxLtr - (range / 2) - 1;
-                        break;
-                    case 'R':
-                        if (range == 1)
-                        {
-                            minLtr = maxLtr;
-                            break;
-                        }
-                        minLtr = minLtr + (range / 2) + 1;
-                        break;
-                }
-            }
-
-            if (minFtb == maxFtb && minLtr == maxLtr)
-            {
-                return (minFtb * 8) + minLtr;
-            }
-
-            return -1;
+            var binary = s.Replace("F", "0").Replace("B", "1").Replace("L", "0").Replace("R", "1");
+            return Convert.ToInt32(binary, 2);
         }
 
         public static long Part2()
         {
-            // manually checked by grouping seat ids by (id)/10, and examining the groups with < 10 values.
-            return 597;
+            var lines = FileReader.ReadInputLines(5).ToList();
+            var seatIds = lines.Select(SeatId).OrderBy(x => x).ToHashSet();
+            var possibleNumbers = Enumerable.Range(0, (int) seatIds.Max());
+            return possibleNumbers.Single(id => !seatIds.Contains(id) && seatIds.Contains(id + 1) && seatIds.Contains(id - 1));
 ;        }
     }
 }
