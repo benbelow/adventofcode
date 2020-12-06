8b898a1c9f656e4467f24340a299ebea0a47e3e8
Ben Below <benjamin.below@softwire.com>
2020-12-06 05:33:15 +0000

    2020: Day 6

--- AdventOfCode.2020/Day6/Day6.cs
+++ AdventOfCode.2020/Day6/Day6.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Common;
+using AdventOfCode.Common.Utils;
 
 namespace AdventOfCode._2020.Day6
 {
@@ -10,13 +12,68 @@ namespace AdventOfCode._2020.Day6
         public static long Part1()
         {
             var lines = FileReader.ReadInputLines(Day).ToList();
-            return -1;
+            var groups = lines.Split("");
+
+            var counts = groups.Select(g =>
+            {
+                var dict = new Dictionary<char, int>();
+                foreach (var s in g)
+                {
+                    foreach (var c in s)
+                    {
+                        if (dict.ContainsKey(c))
+                        {
+                            dict[c]++;
+                        }
+                        else
+                        {
+                            dict[c] = 1;
+                        }
+                    }
+                }
+
+                return dict;
+            }).ToList();
+
+            return counts.Select(dict => dict.Count()).Sum();
         }
 
         public static long Part2()
         {
             var lines = FileReader.ReadInputLines(Day).ToList();
-            return -1;
+            var groups = lines.Split("").ToList();
+
+            var counts = groups.Select(g =>
+            {
+                var dict = new Dictionary<char, int>();
+                foreach (var s in g)
+                {
+                    foreach (var c in s)
+                    {
+                        if (dict.ContainsKey(c))
+                        {
+                            dict[c]++;
+                        }
+                        else
+                        {
+                            dict[c] = 1;
+                        }
+                    }
+                }
+
+                return dict;
+            }).ToList();
+
+            var count = 0;
+            
+            for (int i = 0; i < counts.Count; i++)
+            {
+                var dict = counts[i];
+                var inputs = groups[i];
+                count += dict.Count(x => x.Value == inputs.Count());
+            }
+
+            return count;
         }
     }
 }
--- AdventOfCode.2020/Day6/Day6Tests.cs
+++ AdventOfCode.2020/Day6/Day6Tests.cs
@@ -9,13 +9,16 @@ namespace AdventOfCode._2020.Day6
         [Test]
         public void Part1()
         {
-            Day6.Part1().Should().Be(0);
+            Day6.Part1().Should().NotBe(14959);
+            Day6.Part1().Should().Be(6521);
         }
         
         [Test]
         public void Part2()
         {
-            Day6.Part2().Should().Be(0);
+            Day6.Part2().Should().NotBe(5);
+            Day6.Part2().Should().NotBe(182);
+            Day6.Part2().Should().Be(3305);
         }
     }
 }
