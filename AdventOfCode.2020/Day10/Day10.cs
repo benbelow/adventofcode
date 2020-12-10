9fe7a508f88418255461e50303014872fdb9cfad
Ben Below <benjamin.below@softwire.com>
2020-12-10 06:42:17 +0000

    2020: Day 10

--- AdventOfCode.2020/Day10/Day10.cs
+++ AdventOfCode.2020/Day10/Day10.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Common;
+using NUnit.Framework;
 
 namespace AdventOfCode._2020.Day10
 {
@@ -10,13 +13,158 @@ namespace AdventOfCode._2020.Day10
         public static long Part1()
         {
             var lines = FileReader.ReadInputLines(Day).ToList();
-            return -1;
+            var adapters = lines.Select(int.Parse);
+            var sorted = adapters.OrderBy(x => x).ToList();
+
+            var value = 0;
+            var used = new List<int>();
+            var oneDiffs = 0;
+            var twoDiffs = 0;
+            var threeDiffs = 0;
+            for (int i = 0; i < sorted.Count(); i++)
+            {
+                var current = sorted[i];
+                if (value + 1 == current)
+                {
+                    used.Add(current);
+                    value = current;
+                    oneDiffs++;
+                    continue;
+                }
+                if (value + 2 == current)
+                {
+                    used.Add(current);
+                    value = current;
+                    twoDiffs++;
+                    continue;
+                }
+                
+                if (value + 3 == current)
+                {
+                    used.Add(current);
+                    value = current;
+                    threeDiffs++;
+                    continue;
+                }
+                throw new Exception("no new adapter!");
+                
+            }
+            
+            return oneDiffs * (threeDiffs+1);
         }
 
-        public static long Part2()
+        public static long Part2(List<string> lines = null)
         {
-            var lines = FileReader.ReadInputLines(Day).ToList();
-            return -1;
+            lines ??= FileReader.ReadInputLines(Day).ToList();
+            var adapters = lines.Select(int.Parse).ToList();
+            var sortedAsc = adapters.OrderBy(x => x).ToList();
+            var sortedDesc = adapters.OrderByDescending(x => x).ToList();
+
+            var numberOfWaysOfArriving = new Dictionary<int, long>();
+            
+            for (int i = 0; i < sortedDesc.Count; i++)
+            {
+                var current = sortedDesc[i];
+
+                var arrivals = 0;
+                
+                if (current == 1)
+                {
+                    arrivals = 1;
+                    numberOfWaysOfArriving[current] = arrivals;
+                    continue;
+                }
+                if (sortedDesc.Contains(current - 1))
+                {
+                    arrivals++;
+                }
+                if (sortedDesc.Contains(current - 2))
+                {
+                    arrivals++;
+                }
+                if (sortedDesc.Contains(current - 3))
+                {
+                    arrivals++;
+                }
+
+                numberOfWaysOfArriving[current] = arrivals;
+            }
+            
+            var numberOfWaysOfLeaving = new Dictionary<int, long>();
+            
+            for (int i = 0; i < sortedAsc.Count; i++)
+            {
+                var current = sortedAsc[i];
+
+                var leaveses = 0;
+                
+                if (i == sortedAsc.Count()-1)
+                {
+                    leaveses = 1;
+                    numberOfWaysOfLeaving[current] = leaveses;
+                    continue;
+                }
+                if (sortedDesc.Contains(current + 1))
+                {
+                    leaveses++;
+                }
+                if (sortedDesc.Contains(current + 2))
+                {
+                    leaveses++;
+                }
+                if (sortedDesc.Contains(current + 3))
+                {
+                    leaveses++;
+                }
+
+                numberOfWaysOfLeaving[current] = leaveses;
+            }
+
+
+            var pathLookup = new Dictionary<int, long>{{0, 1}};
+
+            long GetPaths(int adapter, int jump)
+            {
+                var i = adapter - jump;
+                return pathLookup.ContainsKey(i) ? pathLookup[i] : 0;
+            }
+
+            foreach (var adapter in sortedAsc)
+            {
+                var paths = 0L;
+                paths += GetPaths(adapter, 1);
+                paths += GetPaths(adapter, 2);
+                paths += GetPaths(adapter, 3);
+                pathLookup[adapter] = paths;
+            }
+
+
+            return pathLookup.Last().Value;
+        }
+
+        private static Dictionary<int, long> cacheYo = new Dictionary<int, long>();
+        
+        // TOO SLOW
+        private static long CountPaths(Dictionary<int,long> numberOfWays, int value = 0)
+        {
+            if (cacheYo.ContainsKey(value))
+            {
+                return cacheYo[value];
+            }
+            
+            if (!numberOfWays.ContainsKey(value) && value != 0)
+            {
+                cacheYo[value] = 0;
+                return 0;
+            }
+
+            if (value == numberOfWays.Keys.Max())
+            {
+                cacheYo[value] = 1;
+                return 1;
+            }
+
+            return CountPaths(numberOfWays, value + 1) + CountPaths(numberOfWays, value + 2) + CountPaths(numberOfWays, value + 3);
         }
     }
 }
--- AdventOfCode.2020/Day10/Day10Tests.cs
+++ AdventOfCode.2020/Day10/Day10Tests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -10,16 +11,76 @@ namespace AdventOfCode._2020.Day10
         public void Part1()
         {
             var answer = Day10.Part1();
-            answer.Should().NotBe(-1);
-            answer.Should().Be(0);
+            answer.Should().NotBe(2310);
+            answer.Should().Be(2380);
         }
         
         [Test]
         public void Part2()
         {
             var answer = Day10.Part2();
-            answer.Should().NotBe(-2);
+            answer.Should().NotBe(1776893001870606336L);
             answer.Should().Be(0);
+        }
+        
+        [Test]
+        public void Part2_Example1()
+        {
+            var lines = @"16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4";
+            
+            var answer = Day10.Part2(lines.Split("\n").ToList());
+            answer.Should().NotBe(1776893001870606336L);
+            answer.Should().Be(8);
+        }
+        
+        [Test]
+        public void Part2_Example2()
+        {
+            var lines = @"28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3";
+            
+            var answer = Day10.Part2(lines.Split("\n").ToList());
+            answer.Should().NotBe(1776893001870606336L);
+            answer.Should().Be(19208);
         }
     }
 }
