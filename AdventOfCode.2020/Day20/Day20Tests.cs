f9f82b612e6d7c5fdf8c463abf508f7f248dc0f9
Ben Below <benjamin.below@softwire.com>
2020-12-20 12:50:05 +0000

    2020: Day 20 - Transformation code

--- AdventOfCode.2020/Day20/Day20Tests.cs
+++ AdventOfCode.2020/Day20/Day20Tests.cs
@@ -1,3 +1,4 @@
+using System;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -12,6 +13,51 @@ namespace AdventOfCode._2020.Day20
             var answer = Day20.Part1();
             answer.Should().NotBe(-1);
             answer.Should().Be(83775126454273);
+        }
+
+        [Test]
+        public void TransformTests()
+        {
+            const string testInput = @"Tile 999:
+#..
+#..
+##.";
+            var tile = new Tile(testInput.Split(Environment.NewLine));
+
+            tile.ToString().Should().Be(@"
+#..
+#..
+##.".Trim());
+
+            var right90 = tile.Rotate(Rotation.Right90);
+            right90.ToString().Should().Be(@"
+###
+#..
+...".Trim());
+            
+            var right180 = tile.Rotate(Rotation.Right180);
+            right180.ToString().Should().Be(@"
+.##
+..#
+..#".Trim());
+            
+            var left90 = tile.Rotate(Rotation.Left90);
+            left90.ToString().Should().Be(@"
+...
+..#
+###".Trim());
+
+            var horizontalFlip = tile.Flip(Flip2D.Horizontal);
+            horizontalFlip.ToString().Should().Be(@"
+..#
+..#
+.##".Trim());
+            
+            var verticalFlip = tile.Flip(Flip2D.Vertical);
+            verticalFlip.ToString().Should().Be(@"
+##.
+#..
+#..".Trim());
         }
 
         [Test]
--- AdventOfCode.2020/Day20/Tile.cs
+++ AdventOfCode.2020/Day20/Tile.cs
@@ -1,61 +1,165 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using AdventOfCode.Common.Utils;
+using FluentAssertions;
+using Microsoft.Extensions.Primitives;
 
 namespace AdventOfCode._2020.Day20
 {
     internal class Tile
     {
+        // hardcoded for ease, could generalise later...
+        private readonly int Size;
         public long Id { get; set; }
 
         public Rotation Rotation { get; set; } = Rotation.None;
 
-        public readonly List<List<bool>> Spaces = Enumerable.Range(0, 10)
-            .Select(_ => Enumerable.Range(0, 10).Select(_ => false).ToList()).ToList();
+        public readonly List<List<bool>> Spaces;
 
         #region PreCalculatedForPerformance
 
         public readonly Oriented<long> EdgesAsLongs;
 
+
         // options arise from flipping each side
         public readonly Oriented<(long, long)> EdgeOptions;
-        
+
         #endregion
-        
+
         public Tile(IReadOnlyList<string> inputLines)
         {
+            Size = inputLines.Last().Length;
             Id = long.Parse(inputLines[0].Split()[1].Replace(":", ""));
-            for (var y = 0; y < 10; y++)
+
+            Spaces = Init();
+            for (var y = 0; y < Size; y++)
             {
-                for (var x = 0; x < 10; x++)
+                for (var x = 0; x < Size; x++)
                 {
                     Spaces[y][x] = inputLines[y + 1][x] == '#';
                 }
             }
-            
+
             EdgesAsLongs = new Oriented<long>(ordinal => ordinal switch
             {
                 Ordinal.North => EdgeAsLong(Row(0)),
-                Ordinal.South => EdgeAsLong(Row(9)),
+                Ordinal.South => EdgeAsLong(Row(Size - 1)),
                 Ordinal.West => EdgeAsLong(Column(0)),
-                Ordinal.East => EdgeAsLong(Column(9)),
+                Ordinal.East => EdgeAsLong(Column(Size - 1)),
                 _ => throw new ArgumentOutOfRangeException(nameof(ordinal))
             });
-            
+
             EdgeOptions = new Oriented<(long, long)>(ordinal =>
             {
                 var selfLongs = EdgesAsLongs;
                 return ordinal switch
                 {
                     Ordinal.North => (selfLongs.North, Reverse(selfLongs.North)),
-                    Ordinal.South =>(selfLongs.South, Reverse(selfLongs.South)),
-                    Ordinal.West =>(selfLongs.West, Reverse(selfLongs.West)),
-                    Ordinal.East =>(selfLongs.East, Reverse(selfLongs.East)),
+                    Ordinal.South => (selfLongs.South, Reverse(selfLongs.South)),
+                    Ordinal.West => (selfLongs.West, Reverse(selfLongs.West)),
+                    Ordinal.East => (selfLongs.East, Reverse(selfLongs.East)),
                     _ => throw new ArgumentOutOfRangeException(nameof(ordinal))
                 };
             });
-            
+        }
+
+        private Tile(Tile original, Rotation rotation)
+        {
+            Id = original.Id;
+            Spaces = rotation switch
+            {
+                Rotation.None => original.Spaces,
+                Rotation.Right90 => Right90(original.Spaces),
+                Rotation.Right180 => Right90(Right90(original.Spaces)),
+                Rotation.Left90 => Right90(Right90(Right90(original.Spaces))),
+                _ => throw new ArgumentOutOfRangeException(nameof(rotation), rotation, null)
+            };
+        }
+
+        private Tile(long id, List<List<bool>> spaces)
+        {
+            Id = id;
+            Spaces = spaces;
+        }
+
+        public Tile Rotate(Rotation rotation)
+        {
+            var newGrid = rotation switch
+            {
+                Rotation.None => Spaces,
+                Rotation.Right90 => Right90(Spaces),
+                Rotation.Right180 => Right90(Right90(Spaces)),
+                Rotation.Left90 => Right90(Right90(Right90(Spaces))),
+                _ => throw new ArgumentOutOfRangeException(nameof(rotation), rotation, null)
+            };
+
+            return new Tile(Id, newGrid);
+        }
+
+        public Tile Flip(Flip2D flip2D)
+        {
+            var newGrid = flip2D switch
+            {
+                Flip2D.None => Spaces,
+                Flip2D.Horizontal => FlipHorizontal(Spaces),
+                Flip2D.Vertical => FlipVertical(Spaces),
+                _ => throw new ArgumentOutOfRangeException(nameof(flip2D))
+            };
+            return new Tile(Id, newGrid);
+        }
+
+
+        public List<List<bool>> Right90(List<List<bool>> original)
+        {
+            var rotated = Init();
+
+            for (var y = 0; y < Size; y++)
+            {
+                for (var x = 0; x < Size; x++)
+                {
+                    rotated[y][x] = original[Size - x - 1][y];
+                }
+            }
+
+            return rotated;
+        }
+
+
+        public List<List<bool>> FlipHorizontal(List<List<bool>> original)
+        {
+            var rotated = Init();
+
+            for (var y = 0; y < Size; y++)
+            {
+                for (var x = 0; x < Size; x++)
+                {
+                    rotated[y][x] = original[y][Size - x - 1];
+                }
+            }
+
+            return rotated;
+        }
+
+        public List<List<bool>> FlipVertical(List<List<bool>> original)
+        {
+            var rotated = Init();
+            for (var y = 0; y < Size; y++)
+            {
+                for (var x = 0; x < Size; x++)
+                {
+                    rotated[y][x] = original[Size - y - 1][x];
+                }
+            }
+
+            return rotated;
+        }
+
+        private List<List<bool>> Init()
+        {
+            return Enumerable.Range(0, Size)
+                .Select(_ => Enumerable.Range(0, Size).Select(_ => false).ToList()).ToList();
         }
 
         public int Count => Spaces.Sum(row => row.Count(c => c));
@@ -120,6 +224,22 @@ namespace AdventOfCode._2020.Day20
             }
 
             return new Oriented<List<long>>(NeighbourIds);
+        }
+
+        public string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var row in Spaces)
+            {
+                foreach (var item in row)
+                {
+                    sb.Append(item ? '#' : '.');
+                }
+
+                sb.Append(Environment.NewLine);
+            }
+
+            return sb.ToString().Trim();
         }
     }
 }
