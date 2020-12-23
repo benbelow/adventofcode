7dbfd2079a4c4f3a755ffc31982b6cbd91783450
Ben Below <benjamin.below@softwire.com>
2020-12-23 09:55:51 +0000

    2020: Day 23 Part1

--- AdventOfCode.2020/Day23/Day23.cs
+++ AdventOfCode.2020/Day23/Day23.cs
@@ -1,16 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Common;
+using AdventOfCode.Common.Utils;
 
 namespace AdventOfCode._2020.Day23
 {
     public static class Day23
     {
         private const int Day = 23;
-        
+
         public static long Part1(bool isExample = false)
         {
             var lines = FileReader.ReadInputLines(Day, isExample).ToList();
-            return -1;
+            var cups = lines.Single().Select(x => int.Parse(x.ToString())).ToList();
+
+            var result = PlayCrabCubs(cups, 0, 100);
+
+            var indexOf1 = result.IndexOf(1);
+            var cupsFrom1 = result.Skip(indexOf1 + 1).Concat(result.Take(indexOf1));
+            
+            return CupsToLong(cupsFrom1.ToList());
+        }
+
+        public static long CupsToLong(List<int> cups) => long.Parse(cups.Select(x => x.ToString()).ToList().StringsToString());
+        
+        public static List<int> PlayCrabCubs(List<int> cups, int initialCup, int numberOfRounds)
+        {
+            var numberOfCups = cups.Count;
+            var currentIndex = initialCup;
+
+            int WrappedIndex(int index, int toAdd = 0)
+            {
+                return (index + toAdd) % cups.Count;
+            }
+
+            void PlayRound()
+            {
+                var currentValue = cups[WrappedIndex(currentIndex)];
+
+                var indexesToRemove = new[]
+                {
+                    WrappedIndex(currentIndex, 1),
+                    WrappedIndex(currentIndex, 2),
+                    WrappedIndex(currentIndex, 3)
+                };
+                var removedCups = indexesToRemove.Select(i => cups[i]).ToList();
+                foreach (var removedCup in removedCups)
+                {
+                    cups.Remove(removedCup);
+                }
+
+                var destinationValue = currentValue - 1;
+                if (destinationValue == 0)
+                {
+                    destinationValue = numberOfCups;
+                }
+                while (removedCups.Contains(destinationValue))
+                {
+                    destinationValue = (numberOfCups + destinationValue - 1) % numberOfCups;
+                    if (destinationValue == 0)
+                    {
+                        destinationValue = numberOfCups;
+                    }
+                }
+
+                // Reverse so we always add at same index
+                foreach (var removedCup in ((IEnumerable<int>) removedCups).Reverse())
+                {
+                    var destinationIndex = cups.IndexOf(destinationValue) + 1;
+                    cups.Insert(destinationIndex, removedCup);
+                }
+
+                currentIndex = cups.IndexOf(currentValue);
+                currentIndex = WrappedIndex(currentIndex + 1);
+            }
+
+            for (var round = 0; round < numberOfRounds; round++)
+            {
+                PlayRound();
+            }
+
+            return cups;
         }
 
         public static long Part2(bool isExample = false)
--- AdventOfCode.2020/Day23/Day23Tests.cs
+++ AdventOfCode.2020/Day23/Day23Tests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -10,7 +11,17 @@ namespace AdventOfCode._2020.Day23
         public void Part1_Example()
         {
             var answer = Day23.Part1(true);
-            answer.Should().Be(-1);
+            answer.Should().Be(67384529);
+        }
+
+        [Test]
+        public void Part1_SmallExample()
+        {
+            var cups = "389125467".Select(x => int.Parse(x.ToString())).ToList();
+
+            var answer = Day23.CupsToLong(Day23.PlayCrabCubs(cups, 0, 10));
+
+            answer.Should().Be(583741926);
         }
         
         [Test]
@@ -24,7 +35,7 @@ namespace AdventOfCode._2020.Day23
         public void Part1()
         {
             var answer = Day23.Part1();
-            answer.Should().NotBe(-1);
+            answer.Should().NotBe(527861349L);
             answer.Should().Be(0);
         }
         
