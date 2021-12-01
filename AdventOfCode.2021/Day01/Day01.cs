09f5d5da794a50e8b262f06becd56f707ec7b25d
Ben Below <benjamin.below@softwire.com>
2021-12-01 07:37:33 +0000

    2021 Day 1 Part 2

--- AdventOfCode.2021/Day01/Day01.cs
+++ AdventOfCode.2021/Day01/Day01.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Common;
 
@@ -6,20 +7,25 @@ namespace AdventOfCode._2021.Day01
     public static class Day01
     {
         private const int Day = 01;
-        
+
         public static long Part1(bool isExample = false)
         {
             var lines = FileReader.ReadInputLines(Day, isExample).ToList();
             var intLines = lines.Select(int.Parse).ToList();
 
-            var diffs = intLines.Select((value, i) =>
+            return NumberOfIncreases(intLines);
+        }
+
+        private static long NumberOfIncreases(List<int> nums)
+        {
+            var diffs = nums.Select((value, i) =>
             {
                 if (i == 0)
                 {
                     return (int?) null;
                 }
 
-                var previousValue = intLines[i - 1];
+                var previousValue = nums[i - 1];
                 return value - previousValue;
             });
 
@@ -29,7 +35,21 @@ namespace AdventOfCode._2021.Day01
         public static long Part2(bool isExample = false)
         {
             var lines = FileReader.ReadInputLines(Day, isExample).ToList();
-            return -1;
+            var intLines = lines.Select(int.Parse).ToList();
+
+            var windows = intLines.Select((value, i) =>
+            {
+                if (i < 2)
+                {
+                    return (int?) null;
+                }
+
+                var previousValue = intLines[i - 1];
+                var previousValue2 = intLines[i - 2];
+                return value + previousValue + previousValue2;
+            }).Where(x => x != null).Select(x => x.Value).ToList();
+
+            return NumberOfIncreases(windows);
         }
     }
 }
--- AdventOfCode.2021/Day01/Day01Tests.cs
+++ AdventOfCode.2021/Day01/Day01Tests.cs
@@ -17,7 +17,7 @@ namespace AdventOfCode._2021.Day01
         public void Part2_Example()
         {
             var answer = Day01.Part2(true);
-            answer.Should().Be(-1);
+            answer.Should().Be(5);
         }
         
         [Test]
@@ -33,7 +33,7 @@ namespace AdventOfCode._2021.Day01
         {
             var answer = Day01.Part2();
             answer.Should().NotBe(-2);
-            answer.Should().Be(0);
+            answer.Should().Be(1702);
         }
     }
 }
