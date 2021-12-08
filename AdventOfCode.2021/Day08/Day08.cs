7a3c042bf37efd1e3c8b25f66d0a61e8e3ef835e
Ben Below <benjamin.below@softwire.com>
2021-12-08 08:06:49 +0000

    Day 8 Part 2

--- AdventOfCode.2021/Day08/Day08.cs
+++ AdventOfCode.2021/Day08/Day08.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Common;
+using AdventOfCode.Common.Utils;
 
 namespace AdventOfCode._2021.Day08
 {
@@ -20,42 +22,67 @@ namespace AdventOfCode._2021.Day08
                 return finalDigits.Count(d => d .Count is 2 or 4 or 3 or 7);
             });
         }
-
-        // 0 : abcefg (6) 
-        // 1 : cf (2)
-        // 2 : acdeg (5)
-        // 3 : acdfg (6)
-        // 4 : bcdf (4)
-        // 5 : abdfg (5)
-        // 6 : abdefg (6)
-        // 7 : acf (3)
-        // 8 : abcdefg (7)
-        // 9 : abcdfg (6)
         
-        private static IEnumerable<int> GetOutputsForLine(string line)
+        // 8 : abcdefg (7)
+
+        // 1 : cf (2)
+        // 7 : acf (3)
+        // 4 : bcdf (4)
+
+        // 6 : abdefg (6)
+        // 5 : abdfg (5)
+        // 9 : abcdfg (6)
+        // 0 : abcefg (6) 
+        // 3 : acdfg (5)
+
+        // 2 : acdeg (5)
+
+        private static int GetOutputsForLine(string line)
         {
             var splitLine = line.Split("|");
             var watchedDigits = splitLine.First()
                 .Split(" ")
                 .Select(x => x.Trim().ToHashSet())
                 .ToList();
-            var finalDigits = splitLine.Last().Split(" ").Select(x => x.Trim().ToHashSet()).ToList();
+            var finalDigits = splitLine.Last().Split(" ")
+                .Where(x => x.Trim() != "")
+                .Select(x => x.Trim().ToHashSet()).ToList();
 
-            var charMap = new Dictionary<char, char>();
-            var digitMap = new Dictionary<HashSet<char>, int>();
+            var digitMap = new Dictionary<string, int>();
 
-            // some code
+            var one = watchedDigits.Single(d => d.Count == 2);
+            var four = watchedDigits.Single(d => d.Count == 4);
+            var seven = watchedDigits.Single(d => d.Count == 3);
+            var eight = watchedDigits.Single(d => d.Count == 7);
 
-            var one = watchedDigits.SingleOrDefault(d => d.Count == 2);
-            var seven = watchedDigits.SingleOrDefault(d => d.Count == 3);
+            var six = watchedDigits.Single(x => x.Count == 6 && !x.IsSupersetOf(seven));
+            var five = watchedDigits.Single(x => x.Count == 5 && x.IsSubsetOf(six));
+            var nine = watchedDigits.Single(x => x.Count == 6 && x.IsSupersetOf(four));
 
-            return finalDigits.Select(d => digitMap[d]);
+            var zero = watchedDigits.Single(x => x.Count == 6 && !x.SetEquals(nine) && !x.SetEquals(six));
+            var three = watchedDigits.Single(x => x.Count == 5 && x.IsSupersetOf(seven));
+            var two = watchedDigits.Single(x => x.Count == 5 && !x.SetEquals(three)&& !x.SetEquals(five));
+
+            digitMap[zero.ToList().OrderBy(x => x).CharsToString()] = 0;
+            digitMap[one.ToList().OrderBy(x => x).CharsToString()] = 1;
+            digitMap[two.ToList().OrderBy(x => x).CharsToString()] = 2;
+            digitMap[three.ToList().OrderBy(x => x).CharsToString()] = 3;
+            digitMap[four.ToList().OrderBy(x => x).CharsToString()] = 4;
+            digitMap[five.ToList().OrderBy(x => x).CharsToString()] = 5;
+            digitMap[six.ToList().OrderBy(x => x).CharsToString()] = 6;
+            digitMap[seven.ToList().OrderBy(x => x).CharsToString()] = 7;
+            digitMap[eight.ToList().OrderBy(x => x).CharsToString()] = 8;
+            digitMap[nine.ToList().OrderBy(x => x).CharsToString()] = 9;
+
+            var outputs = finalDigits.Select(f => digitMap[f.ToList().OrderBy(f => f).CharsToString()]);
+            return int.Parse(outputs.Select(o => o.ToString().Single()).CharsToString());
         }
 
         public static long Part2(bool isExample = false)
         {
             var lines = FileReader.ReadInputLines(Day, isExample).ToList();
-            return -1;
+            var outputs = lines.Select(GetOutputsForLine);
+            return outputs.Sum();
         }
     }
 }
--- AdventOfCode.2021/Day08/Day08Tests.cs
+++ AdventOfCode.2021/Day08/Day08Tests.cs
@@ -17,7 +17,7 @@ namespace AdventOfCode._2021.Day08
         public void Part2_Example()
         {
             var answer = Day08.Part2(true);
-            answer.Should().Be(-1);
+            answer.Should().Be(61229);
         }
         
         [Test]
@@ -25,7 +25,7 @@ namespace AdventOfCode._2021.Day08
         {
             var answer = Day08.Part1();
             answer.Should().NotBe(-1);
-            answer.Should().Be(0);
+            answer.Should().Be(548);
         }
         
         [Test]
@@ -33,7 +33,7 @@ namespace AdventOfCode._2021.Day08
         {
             var answer = Day08.Part2();
             answer.Should().NotBe(-2);
-            answer.Should().Be(0);
+            answer.Should().Be(1074888);
         }
     }
 }
