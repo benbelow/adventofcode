e7935c5f29812c7bb1c33db19e1b4aa361d27341
Ben Below <benjamin.below@softwire.com>
2021-12-11 05:30:17 +0000

    Part 2

--- AdventOfCode.2021/Day11/Day11.cs
+++ AdventOfCode.2021/Day11/Day11.cs
@@ -125,7 +125,119 @@ namespace AdventOfCode._2021.Day11
         public static long Part2(bool isExample = false)
         {
             var lines = FileReader.ReadInputLines(Day, isExample).ToList();
-            return -1;
+            var grid = lines.Select(l => l.Select(c => int.Parse(c.ToString())).ToList()).ToList();
+
+            var flashCount = 0;
+
+            var width = lines.First().Length;
+            var height = lines.Count;
+
+            bool InBounds((int, int) c)
+            {
+                var (x, y) = c;
+                return x >= 0 && y >= 0 && x < width && y < height;
+            }
+
+            IEnumerable<(int, int)> AllCoords()
+            {
+                for (int y = 0; y < lines.Count; y++)
+                {
+                    for (int x = 0; x < lines.First().Length; x++)
+                    {
+                        yield return (x, y);
+                    }
+                }
+            }
+
+            int Octopus((int, int) c)
+            {
+                if (!InBounds(c))
+                {
+                    throw new Exception("OUT OF BOUND");
+                }
+
+                var (x, y) = c;
+                return grid[y][x];
+            }
+
+            void SetOctopus((int, int) c, int v)
+            {
+                if (!InBounds(c))
+                {
+                    throw new Exception("OUT OF BOUND");
+                }
+
+                var (x, y) = c;
+                grid[y][x] = v;
+            }
+
+            void TryIncrementOctopus((int, int) c)
+            {
+                if (InBounds(c))
+                {
+                    SetOctopus(c, Math.Min(10, Octopus(c) + 1));
+                }
+            }
+
+            List<(int, int)> Neighbours((int, int) c)
+            {
+                var (x, y) = c;
+                return new[]
+                {
+                    (x - 1, y - 1),
+                    (x - 1, y),
+                    (x - 1, y + 1),
+                    (x, y - 1),
+                    (x, y + 1),
+                    (x + 1, y - 1),
+                    (x + 1, y),
+                    (x + 1, y + 1),
+                }.Where(InBounds).ToList();
+            }
+
+            void Tick()
+            {
+                foreach (var (x, y) in AllCoords())
+                {
+                    var coord = (x, y);
+                    TryIncrementOctopus(coord);
+                }
+
+                var flashed = new List<(int, int)>();
+
+                while (AllCoords().Any(c => !flashed.Contains(c) && Octopus(c) > 9))
+                {
+                    var newFlashes = AllCoords().Where(c => !flashed.Contains(c) && Octopus(c) > 9).ToList();
+                    flashCount += newFlashes.Count;
+
+                    foreach (var flash in newFlashes)
+                    {
+                        foreach (var neighbour in Neighbours(flash))
+                        {
+                            TryIncrementOctopus(neighbour);
+                        }
+
+                        flashed.Add(flash);
+                    }
+                }
+
+                foreach (var fullOctopus in AllCoords().Where(c => Octopus(c) > 9))
+                {
+                    SetOctopus(fullOctopus, 0);
+                }
+            }
+
+            var tick = 0;
+            
+            while (true)
+            {
+                Tick();
+                tick++;
+                if (AllCoords().All(c => Octopus(c) == 0))
+                {
+                    return tick;
+                }
+            }
         }
     }
 }
--- AdventOfCode.2021/Day11/Day11Tests.cs
+++ AdventOfCode.2021/Day11/Day11Tests.cs
@@ -32,7 +32,7 @@ namespace AdventOfCode._2021.Day11
         public void Part2_Example()
         {
             var answer = Day11.Part2(true);
-            answer.Should().Be(-1);
+            answer.Should().Be(195);
         }
         
         [Test]
@@ -40,7 +40,7 @@ namespace AdventOfCode._2021.Day11
         {
             var answer = Day11.Part1();
             answer.Should().NotBe(-1);
-            answer.Should().Be(0);
+            answer.Should().Be(1625);
         }
         
         [Test]
@@ -48,7 +48,7 @@ namespace AdventOfCode._2021.Day11
         {
             var answer = Day11.Part2();
             answer.Should().NotBe(-2);
-            answer.Should().Be(0);
+            answer.Should().Be(244);
         }
     }
 }
