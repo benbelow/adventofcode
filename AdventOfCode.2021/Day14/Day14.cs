083d687f9558f082d2c1c47d96464bfa42799ab6
Ben Below <benjamin.below@softwire.com>
2021-12-14 08:29:03 +0000

    Day 14 Part 2

--- AdventOfCode.2021/Day14/Day14.cs
+++ AdventOfCode.2021/Day14/Day14.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Common;
 
@@ -6,20 +8,25 @@ namespace AdventOfCode._2021.Day14
     public static class Day14
     {
         private const int Day = 14;
-        
+
         public static long Part1(bool isExample = false)
         {
             var lines = FileReader.ReadInputLines(Day, isExample).ToList();
 
+            return RunPolymerEfficient(lines, 10);
+        }
+
+        private static long RunPolymer(List<string> lines, int iterations)
+        {
             var polymer = lines.First();
             var rules = lines.Skip(2)
                 .Select(l => l.Split(" -> "))
                 .ToDictionary(s => (s.First().First(), s.First().Last()), s => s.Last());
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < iterations; i++)
             {
                 var pairs = Enumerable.Range(0, polymer.Length - 1)
-                    .Select(i => (polymer[i], polymer[i+1])).ToList();
+                    .Select(i => (polymer[i], polymer[i + 1])).ToList();
                 var insertions = Enumerable.Range(0, polymer.Length - 1)
                     .Select(i =>
                     {
@@ -40,16 +47,142 @@ namespace AdventOfCode._2021.Day14
                         return p + originalC + insertion;
                     });
                 polymer = newPolymer;
+
+                var polymerByChar1 = polymer.GroupBy(x => x).OrderBy(x => x.Count());
+                foreach (var poly in polymerByChar1)
+                {
+                    Console.WriteLine("" + poly.Key + poly.Count());
+                }
+
+                Console.WriteLine(polymer);
+
+                foreach (var pair in pairs.GroupBy(x => x))
+                {
+                    Console.WriteLine("" + pair.Key.Item1 + pair.Key.Item2 + ":" + pair.Count());
+                }
+
+
+                Console.WriteLine("_______");
             }
 
             var polymerByChar = polymer.GroupBy(x => x).OrderBy(x => x.Count());
             return polymerByChar.Last().Count() - polymerByChar.First().Count();
         }
 
+        private static long RunPolymerEfficient(List<string> lines, int iterations)
+        {
+            var polymer = lines.First();
+            var rules = lines.Skip(2)
+                .Select(l => l.Split(" -> "))
+                .ToDictionary(s => (s.First().First(), s.First().Last()), s => s.Last());
+
+            var pairRules = rules.ToDictionary(rule => rule.Key, rule =>
+            {
+                var newPair1 = (rule.Key.Item1, rule.Value.Single());
+                var newPair2 = (rule.Value.Single(), rule.Key.Item2);
+
+                return new[] { newPair1, newPair2 };
+            });
+
+
+            var pairs = Enumerable.Range(0, polymer.Length - 1)
+                .Select(i => (polymer[i], polymer[i + 1])).ToList();
+
+            var pairCounts = pairs.GroupBy(x => x).ToDictionary(p => p.Key, p => (long)p.Count());
+
+            var queuedPairCounts = new Dictionary<(char, char), long>();
+
+            long GetPairCount((char, char) pair) => pairCounts.ContainsKey(pair) ? pairCounts[pair] : 0;
+            long GetQueuedPairCount((char, char) pair) => queuedPairCounts.ContainsKey(pair) ? queuedPairCounts[pair] : 0;
+
+            void QueuePairCountBump((char, char) pair, long toBump)
+            {
+                var existing = GetQueuedPairCount(pair);
+                queuedPairCounts[pair] = existing + toBump;
+            }
+
+            void BumpPairCount((char, char) pair, long toBump)
+            {
+                var existing = GetPairCount(pair);
+                pairCounts[pair] = existing + toBump;
+            }
+
+            void ApplyQueue()
+            {
+                foreach (var pairCountsKey in pairCounts.Keys)
+                {
+                    pairCounts[pairCountsKey] = 0;
+                }
+                
+                foreach (var queuedPairCount in queuedPairCounts)
+                {
+                    BumpPairCount(queuedPairCount.Key, queuedPairCount.Value);
+                }
+
+                queuedPairCounts = new Dictionary<(char, char), long>();
+            }
+
+            for (int i = 0; i < iterations; i++)
+            {
+                foreach (var rule in pairRules)
+                {
+                    var pairsToIncrease = rule.Value;
+                    var numberToIncreaseBy = GetPairCount(rule.Key);
+
+                    foreach (var pairToIncrease in pairsToIncrease)
+                    {
+                        QueuePairCountBump(pairToIncrease, numberToIncreaseBy);
+                    }
+                }
+                
+                ApplyQueue();
+                var charsCount = GetCharCount(polymer, pairCounts);
+                foreach (var cc in charsCount)
+                {
+                    Console.WriteLine("" + cc.Key + cc.Value);
+                }
+                Console.WriteLine("______");
+
+                foreach (var pc in pairCounts.Where(pc => pc.Value != 0))
+                {
+                    Console.WriteLine("" + pc.Key + pc.Value);
+                }
+                Console.WriteLine("______");
+            }
+
+            var countsByC = GetCharCount(polymer, pairCounts);
+
+            return (countsByC.Values.Max() - countsByC.Values.Min()) / 2;
+        }
+
+        private static Dictionary<char, long> GetCharCount(string polymer, Dictionary<(char, char), long> pairCounts)
+        {
+            var firstC = polymer.First();
+            var lastC = polymer.Last();
+
+            var countsByC = new Dictionary<char, long>
+            {
+                { firstC, 1 },
+                { lastC, 1 },
+            };
+            foreach (var pairCount in pairCounts)
+            {
+                var (c1, c2) = pairCount.Key;
+                var existing1 = countsByC.ContainsKey(c1) ? countsByC[c1] : 0;
+                countsByC[c1] = existing1 + pairCount.Value;
+                
+                var existing2 = countsByC.ContainsKey(c2) ? countsByC[c2] : 0;
+                countsByC[c2] = existing2 + pairCount.Value;
+            }
+
+            return countsByC;
+        }
+
+
         public static long Part2(bool isExample = false)
         {
             var lines = FileReader.ReadInputLines(Day, isExample).ToList();
-            return -1;
+            return RunPolymerEfficient(lines, 40);
         }
     }
 }
--- AdventOfCode.2021/Day14/Day14Tests.cs
+++ AdventOfCode.2021/Day14/Day14Tests.cs
@@ -17,7 +17,7 @@ namespace AdventOfCode._2021.Day14
         public void Part2_Example()
         {
             var answer = Day14.Part2(true);
-            answer.Should().Be(-1);
+            answer.Should().Be(2188189693529);
         }
         
         [Test]
@@ -25,7 +25,7 @@ namespace AdventOfCode._2021.Day14
         {
             var answer = Day14.Part1();
             answer.Should().NotBe(-1);
-            answer.Should().Be(0);
+            answer.Should().Be(2170);
         }
         
         [Test]
@@ -33,7 +33,7 @@ namespace AdventOfCode._2021.Day14
         {
             var answer = Day14.Part2();
             answer.Should().NotBe(-2);
-            answer.Should().Be(0);
+            answer.Should().Be(2422444761283L);
         }
     }
 }
