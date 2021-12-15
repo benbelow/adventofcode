f687d58755a6df7c4e969ef8f65dc50e1bc54281
Ben Below <benjamin.below@softwire.com>
2021-12-15 08:54:30 +0000

    Day 15 Part 2

--- AdventOfCode.2021/Day15/Day15.cs
+++ AdventOfCode.2021/Day15/Day15.cs
@@ -11,22 +11,19 @@ namespace AdventOfCode._2021.Day15
         private const int Day = 15;
 
         private static int shortestFullPathSeen = 999999;
-        
+
         class Chiton
         {
             public readonly (int, int) coord;
             public readonly int val;
 
-            private int x => coord.Item1;
-            private int y => coord.Item2;
-
             public bool Visited { get; set; } = false;
             public int tentativeDistance { get; set; }
-            
-            public Chiton((int, int) coord, int val, int width, int height)
+
+            public Chiton((int, int) coord, int val, int width = 0, int height = 0)
             {
-                tentativeDistance = coord == (0,0) ? 0 : int.MaxValue;
-                
+                tentativeDistance = coord == (0, 0) ? 0 : int.MaxValue;
+
                 this.coord = coord;
                 this.val = val;
             }
@@ -37,22 +34,28 @@ namespace AdventOfCode._2021.Day15
             var lines = FileReader.ReadInputLines(Day, isExample).ToList();
             var width = lines.First().Count();
             var height = lines.Count();
-            
+
             var grid = lines.Select(l => l.Select(c => int.Parse(c.ToString())).ToList())
                 .SelectMany((line, y) => line.Select((val, x) => new KeyValuePair<(int, int), Chiton>((x, y), new Chiton((x, y), val, width, height))))
                 .ToDictionary(q => q.Key, q => q.Value);
 
+            return DestNodeTentativeDistance(width, height, grid);
+        }
 
+        private static long DestNodeTentativeDistance(int width, int height, Dictionary<(int, int), Chiton> grid)
+        {
             var target = (width - 1, height - 1);
 
-            var unvisited = grid.Values.ToHashSet();
+            var unvisited = grid.Values.ToList();
 
+            var withTentative = new HashSet<Chiton>();
+            
             bool InBounds((int, int) c) => c.Item1 >= 0 && c.Item2 >= 0 && c.Item1 < width && c.Item2 < height;
 
             void ConsiderNeighbours(Chiton current)
             {
                 var c = current.coord;
-                
+
                 var ncs = new[]
                 {
                     (c.Item1 + 1, c.Item2),
@@ -65,19 +68,22 @@ namespace AdventOfCode._2021.Day15
                 {
                     var neighbour = grid[nc];
                     var newTentative = current.tentativeDistance + neighbour.val;
+                    withTentative.Add(neighbour);
                     neighbour.tentativeDistance = Math.Min(neighbour.tentativeDistance, newTentative);
                 }
 
                 current.Visited = true;
+                unvisited.Remove(current);
+                withTentative.Remove(current);
             }
-            
-            ConsiderNeighbours(grid[(0,0)]);
+
+            ConsiderNeighbours(grid[(0, 0)]);
 
             var destNode = grid[target];
 
             while (!destNode.Visited)
             {
-                var minNode = grid.Values.Where(q => !q.Visited).OrderBy(q => q.tentativeDistance).First();
+                var minNode = withTentative.OrderBy(q => q.tentativeDistance).First();
                 ConsiderNeighbours(minNode);
             }
 
@@ -87,7 +93,24 @@ namespace AdventOfCode._2021.Day15
         public static long Part2(bool isExample = false)
         {
             var lines = FileReader.ReadInputLines(Day, isExample).ToList();
-            return -1;
+            var width = lines.First().Count();
+            var height = lines.Count();
+
+
+            var grid = new Dictionary<(int, int), Chiton>();
+
+            for (int x = 0; x < width * 5; x++)
+            {
+                for (int y = 0; y < height * 5; y++)
+                {
+                    var originalVal = int.Parse(lines[y % height][x % width].ToString());
+                    var newValUnmodified = (originalVal + (y / height) + (x / width));
+                    var newVal = newValUnmodified == 9 ? 9 : newValUnmodified % 9;
+                    grid[(x, y)] = new Chiton((x, y), newVal);
+                }
+            }
+
+            return DestNodeTentativeDistance(width * 5, height * 5, grid);
         }
     }
 }
--- AdventOfCode.2021/Day15/Day15Tests.cs
+++ AdventOfCode.2021/Day15/Day15Tests.cs
@@ -17,7 +17,7 @@ namespace AdventOfCode._2021.Day15
         public void Part2_Example()
         {
             var answer = Day15.Part2(true);
-            answer.Should().Be(-1);
+            answer.Should().Be(315);
         }
         
         [Test]
@@ -25,15 +25,16 @@ namespace AdventOfCode._2021.Day15
         {
             var answer = Day15.Part1();
             answer.Should().NotBe(-1);
-            answer.Should().Be(0);
+            answer.Should().Be(698);
         }
         
         [Test]
+        [Ignore("wow this is slow.")]
         public void Part2()
         {
             var answer = Day15.Part2();
             answer.Should().NotBe(-2);
-            answer.Should().Be(0);
+            answer.Should().Be(3022);
         }
     }
 }
