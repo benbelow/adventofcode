a38e9e72d51f640a16568720e6829efcb15e37fd
Ben Below <benjamin.below@softwire.com>
2021-12-18 11:25:13 +0000

    Explode (works for examples, but I'm not filled with confidence!)

--- AdventOfCode.2021/Day18/Day18.cs
+++ AdventOfCode.2021/Day18/Day18.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using AdventOfCode.Common;
+using NUnit.Framework;
 
 namespace AdventOfCode._2021.Day18
 {
@@ -12,23 +13,34 @@ namespace AdventOfCode._2021.Day18
 
         public class SnailNumber
         {
+            private const int ExplosionCutoffNestingLevel = 4;
+            private const int SplitMaxValueCutoff = 10;
             public SnailNumber Item1 { get; set; }
             public SnailNumber Item2 { get; set; }
 
             public int? RegularNumberValue { get; set; }
+            public bool IsRegular => RegularNumberValue != null;
             public string RawData { get; set; }
 
-            public SnailNumber(string rawData)
+            public int NestingLvl { get; set; }
+            public SnailNumber Parent { get; }
+
+            public bool IsItem1 => Parent.Item1 == this;
+            public bool IsItem2 => Parent.Item2 == this;
+
+            public SnailNumber(string rawData, int nestingLevel = 0, SnailNumber parent = null)
             {
                 RawData = rawData;
-                
+                NestingLvl = nestingLevel;
+                Parent = parent;
+
                 if (int.TryParse(rawData, out var actualNumber))
                 {
                     RegularNumberValue = actualNumber;
                     return;
                 }
-                
-                var nestingLevel = 0;
+
+                var bracketNestingLevel = 0;
                 int commaIndex = 0;
                 for (int i = 1; i < rawData.Length - 1; i++)
                 {
@@ -36,24 +48,62 @@ namespace AdventOfCode._2021.Day18
                     switch (c)
                     {
                         case '[':
-                            nestingLevel++;
+                            bracketNestingLevel++;
                             break;
                         case ']':
-                            nestingLevel--;
+                            bracketNestingLevel--;
                             break;
                         case ',':
-                            if (nestingLevel == 0)
+                            if (bracketNestingLevel == 0)
                             {
                                 commaIndex = i;
                             }
+
                             break;
                     }
                 }
 
                 var rawItem1 = rawData.Substring(1, commaIndex - 1);
                 var rawItem2 = rawData.Substring(commaIndex + 1, rawData.Length - rawItem1.Length - 3);
-                Item1 = new SnailNumber(rawItem1);
-                Item2 = new SnailNumber(rawItem2);
+                Item1 = new SnailNumber(rawItem1, nestingLevel + 1, this);
+                Item2 = new SnailNumber(rawItem2, nestingLevel + 1, this);
+            }
+
+            public override string ToString()
+            {
+                if (IsRegular)
+                {
+                    return RegularNumberValue.Value.ToString();
+                }
+                else
+                {
+                    return $"[{Item1},{Item2}]";
+                }
+            }
+
+            public List<SnailNumber> RegularNumbersList()
+            {
+                if (IsRegular)
+                {
+                    return new List<SnailNumber> { this };
+                }
+
+                return Item1.RegularNumbersList().Concat(Item2.RegularNumbersList()).ToList();
+            }
+
+            public List<SnailNumber> NonRegularNumbersList()
+            {
+                if (IsRegular)
+                {
+                    return new List<SnailNumber>();
+                }
+
+                if (Item1.IsRegular || Item2.IsRegular)
+                {
+                    return new List<SnailNumber> { this }.Concat(Item1.NonRegularNumbersList()).Concat(Item2.NonRegularNumbersList()).ToList();
+                }
+
+                return Item1.NonRegularNumbersList().Concat(Item2.NonRegularNumbersList()).ToList();
             }
 
             public long Magnitude()
@@ -69,9 +119,105 @@ namespace AdventOfCode._2021.Day18
                     var item2Magnitude = Item2.Magnitude();
                     // Console.WriteLine(RawData);
                     // Console.WriteLine("" + item1Magnitude + " | " + item2Magnitude);
-                    
+
                     return item1Magnitude * 3 + item2Magnitude * 2;
                 }
+            }
+
+            public IEnumerable<int> RegularNumbers()
+            {
+                if (RegularNumberValue != null)
+                {
+                    return new[] { RegularNumberValue.Value };
+                }
+
+                return Item1.RegularNumbers().Concat(Item2.RegularNumbers());
+            }
+
+            public int MaxNestingLevel()
+            {
+                return IsRegular ? NestingLvl : Math.Max(Item1.MaxNestingLevel(), Item2.MaxNestingLevel());
+            }
+
+            public bool ShouldExplode() => MaxNestingLevel() >= ExplosionCutoffNestingLevel;
+
+            public bool ShouldSplit() => RegularNumbers().Any(x => x >= SplitMaxValueCutoff);
+
+            public SnailNumber Reduce()
+            {
+                var result = this;
+                var periodicityShouldExplode = true;
+
+                while (result.ShouldExplode() || result.ShouldSplit())
+                {
+                    if (periodicityShouldExplode)
+                    {
+                        result = result.Explode();
+                        periodicityShouldExplode = periodicityShouldExplode;
+                    }
+                    else
+                    {
+                        result = result.Split();
+                        periodicityShouldExplode = !periodicityShouldExplode;
+                    }
+                }
+
+                return result;
+            }
+
+            private SnailNumber Split()
+            {
+                throw new NotImplementedException();
+            }
+
+
+            public SnailNumber Explode()
+            {
+                var nonRegularNumbersList = NonRegularNumbersList();
+                var regularNumbers = RegularNumbersList();
+                
+                var toExplode = nonRegularNumbersList.First(x => x.NestingLvl >= ExplosionCutoffNestingLevel);
+
+                var explodeIndex = nonRegularNumbersList.IndexOf(toExplode);
+                var regularExplodeIndexes = new[] { regularNumbers.IndexOf(toExplode.Item1), regularNumbers.IndexOf(toExplode.Item2) };
+                var regularExplodeIndexLeft = regularExplodeIndexes.Min();
+                var regularExplodeIndexRight = regularExplodeIndexes.Max();
+
+                // All examples has val1 and val2 being regular numbers - not sure what to do if this isn't correct.
+                var val1 = toExplode.Item1.RegularNumberValue.Value;
+                var val2 = toExplode.Item2.RegularNumberValue.Value;
+
+                if (toExplode.IsItem1)
+                {
+                    // right
+                    toExplode.Parent.Item2.RegularNumbersList().First().RegularNumberValue += val2;
+
+                    // left
+                    var prev = regularExplodeIndexLeft != 0 ? regularNumbers[regularExplodeIndexLeft - 1] : null;
+                    if (prev != null)
+                    {
+                        prev.RegularNumberValue += val1;
+                    }
+                }
+
+                if (toExplode.IsItem2)
+                {
+                    // left
+                    toExplode.Parent.Item1.RegularNumbersList().Last().RegularNumberValue += val1;
+
+                    // right
+                    var prev = regularExplodeIndexRight < regularNumbers.Count -1 ? regularNumbers[regularExplodeIndexRight + 1] : null;
+                    if (prev != null)
+                    {
+                        prev.RegularNumberValue += val2;
+                    }
+                }
+
+                toExplode.Item1 = null;
+                toExplode.Item2 = null;
+                toExplode.RegularNumberValue = 0;
+                
+                return this;
             }
         }
 
--- AdventOfCode.2021/Day18/Day18Tests.cs
+++ AdventOfCode.2021/Day18/Day18Tests.cs
@@ -19,6 +19,44 @@ namespace AdventOfCode._2021.Day18
             snailNumber.Magnitude().Should().Be(expected);
         }
         
+        [TestCase("[9,1]","[9,1]")]
+        [TestCase("[[1,2],[[3,4],5]]","[[1,2],[[3,4],5]]")]
+        [TestCase("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]","[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")]
+        [TestCase("[[[[1,1],[2,2]],[3,3]],[4,4]]","[[[[1,1],[2,2]],[3,3]],[4,4]]")]
+        [TestCase("[[[[3,0],[5,3]],[4,4]],[5,5]]","[[[[3,0],[5,3]],[4,4]],[5,5]]")]
+        [TestCase("[[[[5,0],[7,4]],[5,5]],[6,6]]","[[[[5,0],[7,4]],[5,5]],[6,6]]")]
+        [TestCase("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]","[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")]
+        public void SnailNumber_ToString(string rawNumber, string expected)
+        {
+            var snailNumber = new Day18.SnailNumber(rawNumber);
+            snailNumber.ToString().Should().Be(expected);
+        }
+
+        [Test]
+        public void SnailNumber_RegularNumbers()
+        {
+            var snailNumber = new Day18.SnailNumber("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]");
+            snailNumber.RegularNumbers().Should().BeEquivalentTo(new [] {8,7,7,7,8,6,7,7,0,7,6,6,8,7});
+        }
+
+        [Test]
+        public void SnailNumber_NestingLevel()
+        {
+            var snailNumber = new Day18.SnailNumber("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]");
+            snailNumber.MaxNestingLevel().Should().Be(4);
+        }
+
+        [TestCase("[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]")]
+        [TestCase("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]")]
+        [TestCase("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]")]
+        [TestCase("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")]
+        [TestCase("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")]
+        public void Explode(string start, string expected)
+        {
+            var sn = new Day18.SnailNumber(start);
+            sn.Explode().ToString().Should().Be(expected);
+        }
+        
         [Test]
         public void Part1_Example()
         {
